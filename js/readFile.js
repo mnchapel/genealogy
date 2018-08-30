415a6ca535ffab03dd890984cd9ca1b04db0a15c
Marie-Neige Chapel <marieneigecha@gmail.com>
2018-08-30 12:32:46 +0200

    Test script

--- js/readFile.js
+++ js/readFile.js
@@ -1,7 +1,8 @@
 async function readFile(path)
 {
-	await $.get(path, function(response)
+	await $.get(path, async function(response)
 	{
+		console.log("read file: " + response);
 		return response;
 	});
 }
