154254ec6e2fc35ef8d437b533f3fb198b25cc35
Marie-Neige Chapel <marieneigecha@gmail.com>
2018-08-30 14:07:43 +0200

    Test script

--- js/decrypt.js
+++ js/decrypt.js
@@ -1,10 +1,10 @@
-async function decrypt(encrypted)
+async function decrypt(encrypted, password)
 {
 	console.log("in decrypt fun: " + encrypted);
 	var options = {
 		message: await openpgp.message.readArmored(encrypted), // parse encrypted bytes
-		passwords: ['testpassword'],              		// decrypt with password
-		format: 'utf8'                          		// output as Uint8Array
+		passwords: [password], 		// decrypt with password
+		format: 'utf8'         		// output
 	};
 
 	openpgp.decrypt(options).then(function(plainText) {
