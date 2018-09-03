75eda565eb2c59d775ea3b8016be3fb01b29a135
Marie-Neige Chapel <marieneigecha@gmail.com>
2018-09-03 16:26:42 +0200

    Fix mainWindow.js

--- js/mainWindow.js
+++ js/mainWindow.js
@@ -4,7 +4,7 @@ var boxWidth = 100,
 
 var spouseDataJson;
 var treeDataJson = JSON.parse(treeData);
-d3.json("https://raw.githubusercontent.com/mnchapel/genealogy/gh-pages/data/spouseData.json", function(data) { spouseDataJson = data; });
+d3.json("https://raw.githubusercontent.com/mnchapel/genealogy/gh-pages/data/spouseData.json", function(data) { spouseDataJson = data[0]; });
 
 console.log(treeData);
 console.log(spouseDataJson);
