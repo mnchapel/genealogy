797773c153eb02100d8615a0e2df30c3bf893b9a
Marie-Neige Chapel <marieneigecha@gmail.com>
2018-09-04 21:38:38 +0200

    Fix mainWindow.js

--- js/mainWindow.js
+++ js/mainWindow.js
@@ -16,10 +16,11 @@ var zoom = d3.behavior.zoom()
 	.translate([150, 200]);
 
 var svg = d3.select("body").append("svg")
-	.attr('width', "100%")
-	.attr('height', "100%")
+	.attr("width", "100%")
+	.attr("height", "100%")
+	.attr("z-index", "-1")
 	.call(zoom)
-	.append('g')
+	.append("g")
 	.attr("transform", "translate(150,200)");
 
 var nodeWidth  = 220;
